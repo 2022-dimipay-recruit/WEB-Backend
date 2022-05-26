a765756cb437d8b0ae693de77b5e7bb3c4f39665
HyunCha <hyuncha@Beautiful-Dark-Fantasy.local>
2022-05-26 20:40:54 +0900

    feat: implemented question schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,6 @@ export const TokenTypeValues = ['REFRESH', 'ACCESS'];
 export type TokenType = typeof TokenTypeValues[number];
 
 export type HTTPMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
+export type QuestionType = 'anonym' | 'real';
+export type QuestionStatus = 'received' | 'accepted' | 'rejected';
