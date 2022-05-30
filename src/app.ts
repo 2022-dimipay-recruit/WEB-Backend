9e0c79fb32e9cf4d8984bd4fbbb66dd75534f35f
kraccoon-dev <kraccoon@kakao.com>
2022-05-30 15:21:23 +0900

    feat: add jsend middleware

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,46 @@
-import express from 'express';
+import express from 'express'
 
-import cors from 'cors';
-import bearerToken from 'express-bearer-token';
-import helmet from 'helmet';
-import path from 'path';
+import cors from 'cors'
+import bearerToken from 'express-bearer-token'
+import helmet from 'helmet'
+import path from 'path'
+import jsend from 'jsend'
 
-import { attachUserInfo, errorHandler } from './middlewares';
-import { serviceRouter } from './routes';
+import { attachUserInfo, errorHandler } from './middlewares'
+import { serviceRouter } from './routes'
 
 class App {
-  public app: express.Application;
+  public app: express.Application
 
   constructor() {
-    this.app = express();
-    this.initializeMiddlewares();
-    this.initializeRouter();
-    this.initializeErrorHandlers();
+    this.app = express()
+    this.initializeMiddlewares()
+    this.initializeRouter()
+    this.initializeErrorHandlers()
   }
 
   private initializeRouter() {
-    this.app.use('/', serviceRouter);
+    this.app.use('/', serviceRouter)
   }
 
   private initializeMiddlewares() {
-    this.app.use(helmet());
-    this.app.use(cors());
-    this.app.use(express.json());
-    this.app.use(express.static(path.join(__dirname, '/assets')));
-    this.app.use(bearerToken({
-      headerKey: 'Bearer',
-      reqKey: 'token',
-    }));
-    this.app.use(attachUserInfo);
+    this.app.use(helmet())
+    this.app.use(cors())
+    this.app.use(express.json())
+    this.app.use(express.static(path.join(__dirname, '/assets')))
+    this.app.use(
+      bearerToken({
+        headerKey: 'Bearer',
+        reqKey: 'token',
+      })
+    )
+    this.app.use(attachUserInfo)
+    this.app.use(jsend.middleware)
   }
 
   private initializeErrorHandlers() {
-    this.app.use(errorHandler);
+    this.app.use(errorHandler)
   }
 }
 
-export default App;
+export default App
