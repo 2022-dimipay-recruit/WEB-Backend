91fbd73b261b977c390c87efbf759b5de0fec238
kraccoon-dev <kraccoon@kakao.com>
2022-06-01 10:11:35 +0900

    fix: reject on refresh token is not existing

--- src/routes/auth/refresh.controller.ts
+++ src/routes/auth/refresh.controller.ts
@@ -1,9 +1,14 @@
-import { Request, Response } from 'express'
+import { HttpException } from 'exceptions/index'
+import { Request, Response, NextFunction } from 'express'
 import { verify, signTokens } from 'resources/token'
 
 export default function (
-  { cookies: { refreshTOken } }: Request,
-  res: Response
+  { cookies: { refreshToken } }: Request,
+  res: Response,
+  next: NextFunction
 ): void {
-  signTokens(res, verify(refreshTOken))
+  if (typeof refreshToken === 'undefined') {
+    return next(new HttpException(400, 'wrong token'))
+  }
+  signTokens(res, verify(refreshToken))
 }
