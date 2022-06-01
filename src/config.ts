ffc892bc35b36cd184909dbbf56d2daac298402b
kraccoon-dev <kraccoon@kakao.com>
2022-06-01 09:24:29 +0900

    feat: cookie parser

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-const env = dotenv.config();
-if (!env) throw new Error('No env file found');
+const env = dotenv.config()
+if (!env) throw new Error('No env file found')
 
 export default {
   port: process.env.SERVER_PORT!,
   jwtSecret: process.env.JWT_SECRET!,
-};
+  cookieSecret: process.env.COOKIE_SECRET!,
+}
