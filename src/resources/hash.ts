c8221c3a42c4150e6984faea283ac967a2f91c33
kraccoon-dev <kraccoon@kakao.com>
2022-06-01 09:34:46 +0900

    fix: remove comments

--- src/resources/hash.ts
+++ src/resources/hash.ts
@@ -1,21 +1,5 @@
 import { randomBytes, pbkdf2Sync, BinaryLike } from 'crypto'
 
-/* export const createHash = (password: string): {hashPassword: string, salt: string} => {
-  const passwordSalt = Math.random().toString(16).slice(2) + Math.random().toString(36).slice(2);
-  const hashPassword = crypto.createHash('sha512').update(password + passwordSalt).digest('hex');
-
-  return {
-    hashPassword,
-    salt: passwordSalt
-  };
-} */
-
 export const createSalt = (): string => randomBytes(16).toString('hex')
 export const getHash = (password: BinaryLike, salt: BinaryLike): string =>
   pbkdf2Sync(password, salt, 20000, 64, 'sha256').toString('hex')
-
-/* export const getHash = (password: string, salt: string) => {
-  const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
-
-  return hashPassword;
-} */
