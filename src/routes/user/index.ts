5eb62fea14d532c6bffee976d68a81e2a9371352
kraccoon-dev <kraccoon@kakao.com>
2022-06-14 13:49:56 +0900

    feat: user page

---
+++ src/routes/user/index.ts
@@ -0,0 +1,26 @@
+import { createService } from 'routes';
+import checkExistingUser from 'middlewares/checkExistingUser';
+
+import user from './user.controller';
+import list from './list.controller';
+
+export default createService({
+  name: 'user',
+  baseURL: '/user',
+  routes: [
+    {
+      method: 'get',
+      path: '/:name',
+      needAuth: false,
+      middlewares: [checkExistingUser],
+      handler: user,
+    },
+    {
+      method: 'get',
+      path: '/list/:name',
+      needAuth: false,
+      middlewares: [checkExistingUser],
+      handler: list,
+    },
+  ],
+});
