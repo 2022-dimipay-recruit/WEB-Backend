455bdfd560861cf83232ac74292dc7e62a4fc1f8
kraccoon-dev <kraccoon@kakao.com>
2022-06-19 17:59:37 +0900

    fix: apply notification type

--- src/routes/notification/notification.controller.ts
+++ src/routes/notification/notification.controller.ts
@@ -2,17 +2,19 @@ import { HttpException } from 'exceptions/index';
 import prisma from 'resources/db';
 
 import type { Request, Response, NextFunction } from 'express';
+import type { Notification } from 'types';
 
 export default async function (
-  req: Request,
+  req: Request<any, any, any, Notification>,
   res: Response,
   next: NextFunction
 ) {
   const userName = req.user;
+  const { type = 'all' } = req.query;
 
   try {
     const notifications = await prisma.notification.findMany({
-      where: { userName },
+      where: { userName, read: !(type === 'new') },
       select: { message: true, createAt: true },
     });
 
